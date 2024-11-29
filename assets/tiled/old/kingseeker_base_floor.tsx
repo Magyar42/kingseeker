c65c58f761933e4adec036c32bbc41c28d96b0ce
MadeinCanada <kristof.konig13@gmail.com>
2024-11-29 14:29:16 +0000

    Tiled updates

---
+++ assets/tiled/old/kingseeker_base_floor.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="kingseeker_base_floor" tilewidth="64" tileheight="64" tilecount="550" columns="22">
+ <image source="../../Dropbox/Kingseeker/assets/graphics/tilemap/Floor.png" width="1408" height="1600"/>
+ <tile id="244" probability="0.5"/>
+ <tile id="245" probability="0.5"/>
+ <tile id="252" probability="0.15"/>
+ <tile id="264" probability="0.5"/>
+ <tile id="265" probability="0.5"/>
+ <tile id="266" probability="0.5"/>
+ <tile id="267" probability="0.5"/>
+ <tile id="268" probability="0.5"/>
+ <tile id="270" probability="0.1"/>
+ <tile id="271" probability="0.05"/>
+ <tile id="272" probability="0.05"/>
+ <tile id="273" probability="0.15"/>
+</tileset>
